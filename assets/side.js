c45499f050bbcbc10e3eb6d5ad2ab26d8860fe14
irfan <ipratama209@gmail.com>
2020-05-04 11:35:34 +0700

    first commit

---
+++ assets/side.js
@@ -0,0 +1,9 @@
+function openNavbar() {
+  document.getElementById("sideNavigation").style.width = "200px";
+  document.getElementById("main").style.marginLeft= "250px";
+}
+
+function closeNavbar() {
+  document.getElementById("sideNavigation").style.width ="0";
+  document.getElementById("main").style.marginLeft = "0";
+}
